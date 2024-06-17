029449b2e19aa495bfc936acffba861264c35aa1
Johan Smarius <jsmarius@hotmail.com>
2024-06-17 19:35:43 +0200

    Changed to be able to host on Azure

--- DemoAspire.AppHost/Program.cs
+++ DemoAspire.AppHost/Program.cs
@@ -1,20 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var dbPassword = builder.AddParameter("dbPassword", secret: true);
+var dbPassword = builder.AddParameter("dbpassword", secret: true);
 
-var dbServer = builder.AddPostgres("dbServer", password: dbPassword).WithDataVolume().WithPgAdmin();
+var dbServer = builder.AddPostgres("dbserver", password: dbPassword).WithPgAdmin().WithExternalHttpEndpoints();
 var db = dbServer.AddDatabase("db");
 
 var redis = builder.AddRedis("redis");
 
-var eventApi = builder.AddProject<Projects.EventsApi>("eventsapi").WithReference(redis);
-var volunteerApi = builder.AddProject<Projects.VolunteerApi>("volunteerapi").WithReplicas(2);
+var eventApi = builder.AddProject<Projects.EventsApi>("eventsapi").WithReference(redis).WithExternalHttpEndpoints();
+var volunteerApi = builder.AddProject<Projects.VolunteerApi>("volunteerapi").WithReplicas(2).WithExternalHttpEndpoints();
 
 builder.AddProject<Projects.EventApp>("eventapp")
     .WithReference(eventApi)
     .WithReference(volunteerApi)
     .WithReference(redis)
-    .WithReference(db);
+    .WithReference(db)
+    .WithExternalHttpEndpoints();
 
 builder.AddProject<Projects.Postgress_Migrations>("postgress-migrations")
     .WithReference(db);
