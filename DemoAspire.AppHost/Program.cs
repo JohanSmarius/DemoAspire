2395c3a6f9933ca3f7b60d2dcaff2343ffacc3d1
Johan Smarius <jsmarius@hotmail.com>
2024-06-21 18:15:06 +0200

    Added extra instance for the volunteer api

--- DemoAspire.AppHost/Program.cs
+++ DemoAspire.AppHost/Program.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 
 var eventApi = builder.AddProject<Projects.EventsApi>("eventsapi");
-var volunteerApi = builder.AddProject<Projects.VolunteerApi>("volunteerapi");
+var volunteerApi = builder.AddProject<Projects.VolunteerApi>("volunteerapi").WithReplicas(2);
 
 builder.AddProject<Projects.EventApp>("eventapp").WithReference(eventApi).WithReference(volunteerApi);
 
